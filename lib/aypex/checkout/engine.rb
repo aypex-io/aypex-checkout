24eaa85df797301d241edfe71f9e21960108eea7
Matthew Kennedy <m.kennedy@me.com>
2023-01-14 16:07:32 +0000

    Standard rb

--- lib/aypex/checkout/engine.rb
+++ lib/aypex/checkout/engine.rb
@@ -15,27 +15,27 @@ module Aypex
       end
 
       def self.admin_available?
-        @admin_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Admin::Engine')
+        @admin_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("Aypex::Admin::Engine")
       end
 
       def self.api_available?
-        @api_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Api::Engine')
+        @api_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("Aypex::Api::Engine")
       end
 
       def self.cli_available?
-        @cli_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Cli::Engine')
+        @cli_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("Aypex::Cli::Engine")
       end
 
       def self.emails_available?
-        @emails_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Emails::Engine')
+        @emails_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("Aypex::Emails::Engine")
       end
 
       def self.sample_available?
-        @sample_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('AypexSample::Engine')
+        @sample_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("AypexSample::Engine")
       end
 
       def self.storefront_available?
-        @storefront_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Aypex::Storefront::Engine')
+        @storefront_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?("Aypex::Storefront::Engine")
       end
 
       config.to_prepare(&method(:activate).to_proc)
